0b5eff26196619bb1f596bf3012a1cac3952284c
Stover C Mardis <stover.mardis@uvm.edu>
2019-09-02 22:20:03 -0400

    finished Deliverable 01

--- Del01.py
+++ Del01.py
@@ -5,6 +5,7 @@ sys.path.insert(0, '..')
 import Leap
 from pygameWindow import PYGAME_WINDOW
 import random
+import constants
 
 
 def perturb_circle_position():
@@ -21,17 +22,46 @@ def perturb_circle_position():
 
 
 def handle_frame(frame):
+    global x, y, xMin, xMax, yMin, yMax
     hand = frame.hands[0]
     indexFinger = hand.fingers.finger_type(Leap.Finger.TYPE_INDEX)[0]
     distalPhalanx = indexFinger.bone(3)
     tip = distalPhalanx.next_joint
-    print tip
+    x = int(tip[0])
+    y = int(tip[1])
+    if (x < xMin):
+        xMin = x
+    if (x > xMax):
+        xMax = x
+    if (y < yMin):
+        yMin = y
+    if (y > yMax):
+        yMax = y
+    print '(x, y): (' + str(x) + ', ' + str(y) + ')'
+    print 'X limits: (' + str(xMin) + ', ' + str(xMax) + ')'
+    print 'Y limits: (' + str(yMin) + ', ' + str(yMax) + ')'
+
+
+def scale_to_range(val, init_min, init_max, final_min, final_max):
+    init_range = init_max - init_min
+    final_range = final_max -final_min
+    if init_range == 0:
+        new_val = final_min
+    else:
+        new_val = (((val - init_min) * final_range) / init_range) + final_min
+    return int(new_val)
 
 window = PYGAME_WINDOW()
 
 print(window)
 
+xMin = 1000
+xMax = -1000
+yMin = 1000
+yMax = -1000
+
 x, y = 400, 400
+pygameX, pygameY = 400, 400
 
 controller = Leap.Controller()
 
@@ -42,7 +72,9 @@ while True:
     frame = controller.frame()
     if (len(frame.hands) > 0):
         handle_frame(frame)
-    window.draw_black_circle(x, y)
-    perturb_circle_position()
+        pygameX = scale_to_range(x, xMin, xMax, 0, constants.pygameWindowWidth)
+        pygameY = constants.pygameWindowHeight - scale_to_range(y, yMin, yMax, 0, constants.pygameWindowHeight)
+        print 'Coords: (' + str(pygameX) + ', ' + str(pygameY) + ')'
+    window.draw_black_circle(pygameX, pygameY)
     PYGAME_WINDOW.reveal()
 
